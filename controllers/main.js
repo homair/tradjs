81865fb4b7ffe165a6cbd870f6ed1eab467ff523
Sylvain A <sylvain.achard@gmail.com>
2016-11-18 11:13:22 +0100

    add htaccess authentification

--- controllers/main.js
+++ controllers/main.js
@@ -7,6 +7,25 @@ import { getOrderedDocs, updateRoute, deleteRoute } from '../lib/manageDocs'
 
 const router = new Router()
 
+
+// Authentification
+router.all('*', (req, res, next) => {
+  const auth = {login: 'homair', password: 'site2016'} // change this
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+  // Verify login and password are set and correct
+  if (!login || !password || login !== auth.login || password !== auth.password) {
+    res.set('WWW-Authenticate', 'Basic realm="Homair Vacances"')
+    res.status(401).send('Please provide valid credentials...')
+    return
+  }
+  // -----------------------------------------------------------------------
+  // Access granted...
+  console.log('Authentification cleared')
+  next()
+})
+
+
 // -------------------------------------------------
 // Update d'un label
 // -------------------------------------------------
