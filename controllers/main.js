c1f01a33c7b1b58ca17b95b8590be5460b1d100b
Etienne VOILLIOT <etiennevoilliot@homair.com>
2017-04-06 15:17:02 +0200

    amélioration basic auth dev

--- controllers/main.js
+++ controllers/main.js
@@ -12,22 +12,24 @@ const router = new Router()
 // HTTP authentication.
 // -----------------------------------------------------------------------
 // If current environnement doesn't correspond to this list.
-router.all('*', (req, res, next) => {
-  // If the current client IP isn't authorized.
-  if (config.authorizedIps.indexOf(req.ip) === -1) {
-    const auth = {login: 'homair', password: 'site2016'} // change this
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
-    // Verify login and password are set and correct
-    if (!login || !password || login !== auth.login || password !== auth.password) {
-      res.set('WWW-Authenticate', 'Basic realm="Homair Vacances"')
-      res.status(401).send('Please provide valid credentials...')
-      return
+if (['dev', 'test'].indexOf(process.env.NODE_ENV) === -1) {
+  router.all('*', (req, res, next) => {
+    // If the current client IP isn't authorized.
+    if (config.authorizedIps.indexOf(req.ip) === -1) {
+      const auth = {login: 'homair', password: 'site2016'} // change this
+      const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+      const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+      // Verify login and password are set and correct
+      if (!login || !password || login !== auth.login || password !== auth.password) {
+        res.set('WWW-Authenticate', 'Basic realm="Homair Vacances"')
+        res.status(401).send('Please provide valid credentials...')
+        return
+      }
     }
-  }
 
-  next()
-})
+    next()
+  })
+}
 // -----------------------------------------------------------------------
 
 // -------------------------------------------------
