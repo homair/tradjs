93dc00f96b438ba3e5cebd9718ef609bb9111e96
Etienne VOILLIOT <etiennevoilliot@homair.com>
2017-04-06 14:46:40 +0200

    maj basic auth

--- controllers/main.js
+++ controllers/main.js
@@ -8,31 +8,28 @@ import { getOrderedDocs, updateRoute, deleteRoute } from '../lib/manageDocs'
 
 const router = new Router()
 
-
 // -----------------------------------------------------------------------
 // HTTP authentication.
 // -----------------------------------------------------------------------
 // If current environnement doesn't correspond to this list.
-if (['dev', 'test', 'production'].indexOf(process.env.NODE_ENV) === -1) {
-  router.all('*', (req, res, next) => {
-    // If the current client IP isn't authorized.
-    if (config.authorizedIps.indexOf(req.ip) === -1) {
-      const auth = {login: 'homair', password: 'site2016'} // change this
-      const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-      const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
-      // Verify login and password are set and correct
-      if (!login || !password || login !== auth.login || password !== auth.password) {
-        res.set('WWW-Authenticate', 'Basic realm="Homair Vacances"')
-        res.status(401).send('Please provide valid credentials...')
-        return
-      }
+router.all('*', (req, res, next) => {
+  console.log('ip=', req.ip);
+  // If the current client IP isn't authorized.
+  if (config.authorizedIps.indexOf(req.ip) === -1) {
+    const auth = {login: 'homair', password: 'site2016'} // change this
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+    // Verify login and password are set and correct
+    if (!login || !password || login !== auth.login || password !== auth.password) {
+      res.set('WWW-Authenticate', 'Basic realm="Homair Vacances"')
+      res.status(401).send('Please provide valid credentials...')
+      return
     }
+  }
 
-    next()
-  })
-}
+  next()
+})
 // -----------------------------------------------------------------------
-
 
 // -------------------------------------------------
 // Update d'un label
