942b8517b0968957a17e80ba3c99c413937d3abc
Etienne VOILLIOT <etiennevoilliot@homair.com>
2018-07-17 11:48:53 +0200

    Ajouté le port dans la connexion mongodb de dev, car en mongo v4, il sera obligatoire dans la chaine de connexion

--- src/config/index.js
+++ src/config/index.js
@@ -3,48 +3,48 @@ const config = {
   port: process.env.PORT || 3002,
   db: {
     mongodb: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/honodblive',
-      dbname: 'honodblive'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/honodblive',
+      dbname: 'honodblive',
     },
-    root_collection: process.env.ROOT_COLLECTION || 'i18next_flat'    // ### => i18next | i18next_flat
+    root_collection: process.env.ROOT_COLLECTION || 'i18next_flat', // ### => i18next | i18next_flat
   },
   // Ips authorized to access to exports.
   // No HTTP auth for those.
   authorizedIps: ['127.0.0.1', '195.25.103.29'],
   langs: {
-    'fr': {
-      'label': 'French',
-      'order': 0
+    fr: {
+      label: 'French',
+      order: 0,
     },
-    'en': {
-      'label': 'English',
-      'order': 1
+    en: {
+      label: 'English',
+      order: 1,
     },
-    'de': {
-      'label': 'German',
-      'order': 2
+    de: {
+      label: 'German',
+      order: 2,
     },
-    'es': {
-      'label': 'Spanish',
-      'order': 3
+    es: {
+      label: 'Spanish',
+      order: 3,
     },
-    'it': {
-      'label': 'Italien',
-      'order': 4
+    it: {
+      label: 'Italien',
+      order: 4,
     },
-    'nl': {
-      'label': 'Dutch',
-      'order': 5
+    nl: {
+      label: 'Dutch',
+      order: 5,
     },
-    'pl': {
-      'label': 'Polish',
-      'order': 5
-    }
+    pl: {
+      label: 'Polish',
+      order: 5,
+    },
   },
   // order to sort docs
   lang_order: ['fr', 'en', 'de', 'es', 'it', 'nl', 'pl'],
-  flat_collection: process.env.FLAT_COLLECTION === 'true',  // ### => false | true
-  translationNamespace: 'translation'
+  flat_collection: process.env.FLAT_COLLECTION === 'true', // ### => false | true
+  translationNamespace: 'translation',
 }
 
 export default config
